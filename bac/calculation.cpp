d9f04de33bc6594de7d565da28df5be19e7970c5
l3acon <l3acon@gmail.com>
2014-06-24 21:35:17 -0600

    updated calculator to include unit conversions

--- bac/calculation.cpp
+++ bac/calculation.cpp
@@ -7,38 +7,40 @@ using namespace std;
 int main( )
 {
   // BEGIN personal data
-  float body_water_in_blood = 0.806;
+  const float body_water_in_blood = 0.806;
 
-  float ethenol_conversion = 1.2;
+  const float ethenol_conversion = 1.2; 
 
   float body_weight = 100.0; 	//weight in kilograms
   char gender = 'F';
-  float body_water_constant = 0.58; //for females
-  float metabolism = 0.017;
+  float body_water_constant = 0.58; //constant for females
+  float metabolism = 0.017; // constant for females
   if (gender == 'F')
   {
-	body_water_constant = 0.49;
-	metabolism = 0.015;
+	body_water_constant = 0.49; // constant for males
+	metabolism = 0.015; // constant for males
   }
   const float constants = (body_water_in_blood * ethenol_conversion ) / ( body_weight * body_water_constant);
   // END personal data
   
+  // example numbers 
   float standard_drinks = 1.0;	// 10 grams of ethenol
   float duration = 1.23; 	// duration in hours 
 
-  float current_ebac;
- 
-  long current_time ;
-  long start_time = 0;
+  const long start_time = 0;
   
-  //conversions
+  //conversions, (example numbers)
   float beverage_pc_alcohol = 0.0720;
   float beverage_size_in_fluid_ounces = 12.0;
+  // using constant values of 29.57 grams in a US fluid oz 
+  // and 14 grams in a standard drink
   float total_std_drinks = beverage_pc_alcohol * (beverage_size_in_fluid_ounces * 29.57)/14;
-
+  
+  // END conversions
   vector<float> ebacs;
 
   // calculations  
+  float current_ebac, current_time;
   current_time = 1; 
   current_ebac = constants * total_std_drinks - ( metabolism * (current_time - start_time ));
   
